f8d1d24e30228ae58aca17ab2dce7331c67577f7
tanmay <tanmayshukla252@gmail.com>
2025-04-22 21:07:11 +0530

    fewUpdate

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,48 @@
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import { FaPlusCircle } from "react-icons/fa";
-import { FiMinusCircle } from "react-icons/fi";
+import './App.css'
 
 function App() {
-  const [ShowText1, setShowdata1] = useState("")
-  const [ShowText2, setShowdata2] = useState("")
-  const [ShowText3, setShowdata3] = useState("")
+  const [showText, setDataShow] = useState(null);
 
- function openDiv1() {
-  setShowdata1(show=>!show)
-  setShowdata2(false)
-  setShowdata3(false)
-  
- }
- function openDiv2() {
-  setShowdata2(show=>!show)
-  setShowdata1(false)
-  setShowdata3(false)
-  
- }
- function openDiv3() {
-  setShowdata3(show=>!show)
-  setShowdata1(false)
-  setShowdata2(false)
-  
- }
-
-  
+  const toggleSection = (section) => {
+    setDataShow(prev => (prev === section ? null : section));
+  };
 
   return (
     <>
-    <div id='parent'>
+      <div id='parent'>
         <div id='head'>
           <div><h3>heading1</h3></div>
-          <span onClick={openDiv1}>{ShowText1?<FiMinusCircle />:<FaPlusCircle />}</span>
+          <span onClick={() => toggleSection(1)}><FaPlusCircle /></span>
         </div>
-        <p id='para' style={{ display: ShowText1 ? 'block' : 'none' }}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique optio rerum asperiores ut assumenda ex rem fuga quaerat, unde omnis!
+        <p id='para' style={{ display: showText === 1 ? 'block' : 'none' }}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique optio rerum asperiores ut assumenda ex rem fuga quaerat, unde omnis!
         </p>
       </div>
 
       <div id='parent'>
         <div id='head'>
           <div><h3>heading2</h3></div>
-          <span onClick={openDiv2}>{ShowText2?<FiMinusCircle />:<FaPlusCircle />} </span>
+          <span onClick={() => toggleSection(2)}><FaPlusCircle /></span>
         </div>
-        <p id='para' style={{ display: ShowText2 ? 'block' : 'none' }}>
-       Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique optio rerum asperiores ut assumenda ex rem fuga quaerat, unde omnis!
+        <p id='para2' style={{ display: showText === 2 ? 'block' : 'none' }}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique optio rerum asperiores ut assumenda ex rem fuga quaerat, unde omnis!
         </p>
       </div>
 
       <div id='parent'>
         <div id='head'>
           <div><h3>heading3</h3></div>
-          <span onClick={openDiv3}>{ShowText3?<FiMinusCircle />:<FaPlusCircle />}</span>
+          <span onClick={() => toggleSection(3)}><FaPlusCircle /></span>
         </div>
-        <p id='para' style={{ display: ShowText3 ? 'block' : 'none' }}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique optio rerum asperiores ut assumenda ex rem fuga quaerat, unde omnis!
+        <p id='para3' style={{ display: showText === 3 ? 'block' : 'none' }}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique optio rerum asperiores ut assumenda ex rem fuga quaerat, unde omnis!
         </p>
       </div>
     </>
   )
 }
 
-export default App
+export default App;
